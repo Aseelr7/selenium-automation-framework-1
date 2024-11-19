431e22a1394ec129769588b1c47c26032656cff8
aseelr7 <aseelr95@gmail.com>
2024-11-19 16:39:49 -0500

    ConfigManager and DriverFactory done.

--- src/main/java/config/ConfigManager.java
+++ src/main/java/config/ConfigManager.java
@@ -1,4 +1,53 @@
 package config;
 
+import enums.Environment;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigManager {
+
+    public static final Properties properties = new Properties();
+    public static final Logger logger = LogManager.getLogger(ConfigManager.class);
+
+    static{
+        loadProperties();
+    }
+
+
+    private static void loadProperties(){
+
+        try{
+            FileInputStream baseConfig = new FileInputStream("src/main/resources/config/config.properties");
+            properties.load(baseConfig);
+            baseConfig.close();
+
+            //Load The Environment
+            Environment env = Environment.valueOf(properties.getProperty("env").toUpperCase());
+            String envConfigPath = "src/main/resources/config/" + env.name().toLowerCase() +"-config.properties";
+
+            FileInputStream envConfig = new FileInputStream(envConfigPath);
+            properties.load(envConfig);
+            envConfig.close();
+
+            logger.info("Configuration loaded for environment " + env);
+
+        } catch (IOException e){
+            logger.error("Failed to load Configuration" + e);
+            throw new RuntimeException("Configuration Loading process failed");
+        }
+
+
+    }
+
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+
+    public static Environment getProperty(String key, String defaultValue){
+        return Environment.valueOf(properties.getProperty("env").toUpperCase());
+    }
 }
---
+++ src/test/java/tests/base/BrowserTest.java
@@ -0,0 +1,17 @@
+package tests.base;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class BrowserTest extends base.BaseTest {
+
+    @Test
+    public void myBrowserTest(){
+        String expectedUrl = "https://www.facebook.com/";
+        String actualUrl = driver.getCurrentUrl();
+
+        logger.info("Verifying that the browser is opened and navigating to the correct url");
+
+        Assert.assertEquals(actualUrl, expectedUrl, "failed!");
+    }
+}
